5d700f880fe694530a1a5cd00aad6bf2823e57a3
arthas_sjtu <arthas_sjtu@163.com>
2017-06-02 23:34:29 +0800

    remove bug for deadLock

--- src/main/java/io/openmessaging/demo/AsyncLogging.java
+++ src/main/java/io/openmessaging/demo/AsyncLogging.java
@@ -22,43 +22,57 @@ import java.util.concurrent.locks.ReentrantLock;
 public class AsyncLogging implements Runnable{
     //use one thread to manage multiple files
     public static CountDownLatch endSignal;
-    private static final int blockingSize = 1024*1024;//2MB
-    public static final int fileMagicNumber = 27149;
-    ByteBuffer currentBuffer;
-    ByteBuffer nextBuffer;
-    LinkedList<ByteBuffer> buffers_;
+    private static final int blockingSize = 1024*1024*2;//2MB
+    public static final int fileMagicNumber = 1127149;
+
     boolean running_;
 
     String filePath;
 
     Lock lock;
     Condition condition;
+    Condition condition4Append;
 
+    ArrayList<ByteBuffer> producerBufferList,fileBufferList,BackUpList,tmp;
+    ByteBuffer currentBuffer;
     AsyncLogging(String parent,String fileName){
         this.filePath = parent+fileName + AsyncLogging.fileMagicNumber;
         running_ = true;
 
         lock  = new ReentrantLock();
         condition = lock.newCondition();
+        condition4Append = lock.newCondition();
+
+        producerBufferList = new ArrayList<>();
+        fileBufferList = new ArrayList<>();
+        BackUpList = new ArrayList<>();
+
         currentBuffer = ByteBuffer.allocate(blockingSize);
-        nextBuffer = ByteBuffer.allocate(blockingSize);
-        buffers_ = new LinkedList();
+        producerBufferList.add(currentBuffer);
+        producerBufferList.add(ByteBuffer.allocate(blockingSize));
+        //producerBufferList.add(ByteBuffer.allocate(blockingSize));
     }
 
     public void append(byte[] byteArray,int len){
         lock.lock();
-        if (currentBuffer.remaining()>len){
-            currentBuffer.put(byteArray);
-        }else{
-            buffers_.add(currentBuffer);
-            if (nextBuffer != null){
-                currentBuffer = nextBuffer;
-                nextBuffer = null;
+        if (producerBufferList.size()>0){
+            currentBuffer = producerBufferList.get(0);
+            if (currentBuffer.remaining()>len){
+                currentBuffer.put(byteArray);
             }else{
-                currentBuffer = ByteBuffer.allocate(blockingSize);
+                BackUpList.add(currentBuffer);
+                producerBufferList.remove(0);
+                condition.signal();
             }
-            currentBuffer.put(byteArray);
-            condition.signal();
+        }else{
+            try {
+                condition4Append.await();
+                lock.unlock();
+                append(byteArray,len);
+                return;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         lock.unlock();
     }
@@ -70,14 +84,8 @@ public class AsyncLogging implements Runnable{
         lock.unlock();
     }
     //variables for thread
-    ByteBuffer newBuffer1 = ByteBuffer.allocate(blockingSize);
-    ByteBuffer newBuffer2 = ByteBuffer.allocate(blockingSize);
-    ByteBuffer tmpBuffer;
-    LinkedList<ByteBuffer> tmp;
-    LinkedList<ByteBuffer> buffersToWrite = new LinkedList();
     FileOutputStream out = null;
 
-    public static AtomicInteger i = new AtomicInteger(0);
     @Override
     public void run() {
         try {
@@ -89,26 +97,34 @@ public class AsyncLogging implements Runnable{
         while (running_){
             //start of synchronize
             lock.lock();
-            if (buffers_.size() == 0){
-                try {
-                    //反正最后强制flush也不用三秒刷新一次了。。
-                    //condition.await(3000, TimeUnit.MILLISECONDS);
-                    condition.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            try {
+                condition.await();
+                for (ByteBuffer byteBuffer:BackUpList){
+                    fileBufferList.add(byteBuffer);
                 }
+                BackUpList.clear();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            exchangeBuffer();
             lock.unlock();
             //End of synchronize
-            if (buffersToWrite.size() == 0){continue;}
+
             writeFile();
+
+            lock.lock();
+
+            for (ByteBuffer byteBuffer:fileBufferList){
+                producerBufferList.add(byteBuffer);
+            }
+            fileBufferList.clear();
+            condition4Append.signal();
+            lock.unlock();
         }
-        exchangeBuffer();
-        if (buffersToWrite.size() != 0){
-            writeFile();
-        }
+
         try {
+            for (ByteBuffer byteBuffer: producerBufferList){
+                out.write(byteBuffer.array(),0,byteBuffer.position());
+            }
             out.flush();
             out.close();
         } catch (IOException e) {
@@ -117,47 +133,15 @@ public class AsyncLogging implements Runnable{
         endSignal.countDown();
     }
 
-    private void exchangeBuffer(){
-        buffers_.add(currentBuffer);
-        currentBuffer = newBuffer1;
-        newBuffer1 = null;
-        tmp = buffersToWrite;buffersToWrite = buffers_;buffers_=tmp;//swap buffers_ and buffersToWrite
-        if (nextBuffer == null){
-            nextBuffer = newBuffer2;
-            newBuffer2 = null;
-        }
-    }
     private void writeFile(){
-        try {
-            for (int i=0;i<buffersToWrite.size();i++){
-                tmpBuffer = buffersToWrite.get(i);
-                out.write(tmpBuffer.array(),0,tmpBuffer.position());
+        try{
+            for (ByteBuffer byteBuffer:fileBufferList){
+                out.write(byteBuffer.array(),0,byteBuffer.position());
+                byteBuffer.clear();
             }
             out.flush();
-        } catch (IOException e) {
+        }catch (Exception e){
             e.printStackTrace();
         }
-
-        if (newBuffer1 == null){
-            newBuffer1 = buffersToWrite.pop();
-            newBuffer1.clear();
-        }
-
-        if (newBuffer2 == null){
-            newBuffer2 = buffersToWrite.pop();
-            newBuffer2.clear();
-        }
-        buffersToWrite.clear();
-    }
-
-
-    //加入这部分是因为byteBuffer的putInt方式与一般的putInt方式不一样，一个是大端，一个是小端。至于哪个是大端哪个是小端我就不知道了。。
-    private static byte[] int2byte(int res) {
-        byte[] targets = new byte[4];
-        targets[0] = (byte) (res >>> 24);// 最低位
-        targets[1] = (byte) ((res >> 16) & 0xff);// 次低位
-        targets[2] = (byte) ((res >> 8) & 0xff);// 次高位
-        targets[3] = (byte) (res & 0xff);// 最高位,无符号右移。
-        return targets;
     }
 }
