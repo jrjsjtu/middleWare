d09999577a6142fb902599e1150a33561f9698ea
arthas_sjtu <arthas_sjtu@163.com>
2017-06-03 23:04:03 +0800

    remove bug for deadLock

--- src/main/java/io/openmessaging/tester/Constants.java
+++ src/main/java/io/openmessaging/tester/Constants.java
@@ -5,9 +5,9 @@ public class Constants {
     //73723ezor7
     public final static String STORE_PATH = System.getProperty("store.path", "/home/jrj/Desktop/files/");
     public final static int PRO_NUM = Integer.valueOf(System.getProperty("pro.num", "10"));
-    public final static int CON_NUM = Integer.valueOf(System.getProperty("con.num", "120"));
+    public final static int CON_NUM = Integer.valueOf(System.getProperty("con.num", "10"));
     public final static String PRO_PRE = System.getProperty("pro.pre","PRODUCER_");
-    public final static int PRO_MAX = Integer.valueOf(System.getProperty("pro.max","50000"));
+    public final static int PRO_MAX = Integer.valueOf(System.getProperty("pro.max","400000"));
     public final static String CON_PRE = System.getProperty("con.pre", "CONSUMER_");
     public final static String TOPIC_PRE = System.getProperty("topic.pre", "TOPIC_");
     public final static String QUEUE_PRE = System.getProperty("topic.pre", "QUEUE_");
